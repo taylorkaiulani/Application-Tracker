fba2917c17213783ea43a7b216f7f589d6ef6988
taylorkaiulani <taylorkaiulani@hotmail.com>
2022-06-11 15:12:06 -0700

    Added basic CRUD functions to model file in backend

--- Server/applications-model.mjs
+++ Server/applications-model.mjs
@@ -13,4 +13,63 @@ db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
+const applicationSchema = mongoose.Schema({
+    companyName:        {type: String, required: true},
+    submissionDate:     {type: String, required: true},
+    resumeVer:          {type: Number, required: false},
+    onlineAssess:       {type: Boolean, required: false},
+    phoneScreen:        {type: Boolean, required: false},
+    interview:          {type: Boolean, required: false},
+    interviewDate:      {type: String, required: false},
+    offerReceived:      {type: Boolean, required: false},
+    offerAmount:        {type: Boolean, required: false},
+    notes:              {type: String, required: false}
+});
+
+const Application = mongoose.model("Application", applicationSchema);
+
+const createApp = async (company, subDate, resume, oa, phone, interview, intDate, offer, amount, notes) => {
+    const application = new Application(
+        {   companyName: company, 
+            submissionDate: subDate, 
+            resumeVer: resume, 
+            onlineAssess: oa, 
+            phoneScreen: phone,
+            interview: interview,
+            interviewDate: intDate,
+            offerReceived: offer,
+            offerAmount: amount,
+            notes: notes 
+        });
+    return application.save();
+}
+
+const findApps = async () => {
+    const query = Application.find();
+    return query.exec();
+}
+
+const replaceApp = async (id, company, subDate, resume, oa, phone, interview, intDate, offer, amount, notes) => {
+    const result = await Application.findByIdAndUpdate({ _id: id }, 
+        {   companyName: company, 
+            submissionDate: subDate, 
+            resumeVer: resume, 
+            onlineAssess: oa, 
+            phoneScreen: phone,
+            interview: interview,
+            interviewDate: intDate,
+            offerReceived: offer,
+            offerAmount: amount,
+            notes: notes 
+        },
+        {new: true});
+    
+    return result;
+}
+
+const deleteApp = async (id) => {
+    const result = await Application.deleteOne({ _id: id });
+    return result.deletedCount;
+}
+
 export{}
